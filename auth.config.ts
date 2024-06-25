ffbe52b73400ad4b2189cf4890cd6d8f0ce1135a
Leo Li <leoli_082000@hotmail.com>
2024-06-25 00:15:44 -0400

    Fix the auth config and next.config?

--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,14 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
+  providers: [
+    // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
+    // while this file is also used in non-Node.js environments
+    // Hopefully moving this to the top of the file will fix my deployment issue
+  ],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
@@ -17,5 +22,4 @@ export const authConfig = {
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
