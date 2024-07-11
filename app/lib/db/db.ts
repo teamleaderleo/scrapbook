30f0399a69b4d90a9fb0b8dceab46bb933b7bfbe
Leo Li <leoli_082000@hotmail.com>
2024-07-10 22:43:22 -0400

    Finish setting up drizzle

--- app/lib/db/db.ts
+++ app/lib/db/db.ts
@@ -3,7 +3,11 @@ import { Pool } from 'pg';
 import * as schema from './schema';
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_NAME,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 export const db = drizzle(pool, { schema });
---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './app/lib/db/schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql', // 'postgresql' | 'mysql' | 'sqlite'
+  dbCredentials: {
+    host: process.env.POSTGRES_HOST!,
+    user: process.env.POSTGRES_USER!,
+    password: process.env.POSTGRES_PASSWORD!,
+    database: process.env.POSTGRES_DATABASE!,
+  },
+});
