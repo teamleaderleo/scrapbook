57ea6dd43520adc70516a70abc178ba35292c721
Leo Li <leoli_082000@hotmail.com>
2024-07-18 19:21:55 -0400

    add toast to root

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { inter } from '@/components/ui/fonts';
 import { Metadata } from 'next';
 import { ServiceWorkerRegistration } from '@/components/ui/service-worker-registration';
 import { ReactQueryProvider } from '@/components/query-client-provider';
- 
+import { Toaster } from "@/components/ui/toaster";
+
 export const metadata: Metadata = {
   title: {
     template: '%s | Setzen',
@@ -21,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ReactQueryProvider>
-        <body className={`${inter.className} antialiased`}>{children}</body>
+        <body className={`${inter.className} antialiased`}>
+          {children}
+          <Toaster />
+        </body>
         <ServiceWorkerRegistration />
       </ReactQueryProvider>
     </html>
