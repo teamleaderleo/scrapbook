63bf1125440f98e780980a3440372a979a0b0881
Leo Li <leoli_082000@hotmail.com>
2024-07-28 11:28:49 -0400

    prepare for prepare usage

---
+++ app/lib/hooks/usePreparation.ts
@@ -0,0 +1,8 @@
+import { useQuery } from "react-query";
+
+function useRelatedData(type: 'project' | 'artifact', id: string) {
+//   return useQuery(['relatedData', type, id], () => fetchRelatedData(type, id), {
+//     enabled: !!id,
+// use a prepare()
+//   });
+}
