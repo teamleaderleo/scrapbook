a3c2c6e7fc015ba2bdd9f669c9c3b35b2e7beeec
Leo Li <leoli_082000@hotmail.com>
2025-06-04 01:38:19 -0400

    try 100vh

--- components/three-carousel/scene-3d.tsx
+++ components/three-carousel/scene-3d.tsx
@@ -48,7 +48,7 @@ export default function Scene3D() {
   if (!mounted) return null;
   
   return (
-    <div style={{ width: '100%', height: '100%' }}>
+    <div style={{ width: '100%', height: '100vh' }}>
       <Canvas camera={{ position: [3, 3, 3] }}>
         <ScrollControls pages={3} infinite={true}>
           <DragPreventer />
