a633c26c0a34adf8ce404fb1f0928b63568307fe
Leo Li <leoli_082000@hotmail.com>
2025-09-28 02:26:06 -0400

    add suspense since we're using usesearchparams

--- app/space/page.tsx
+++ app/space/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { SpaceView } from "@/components/space/space-view";
 
 export default function SpacePage() {
-  return <SpaceView />;
+  return (
+    <Suspense fallback={<div className="p-4">Loading…</div>}>
+      <SpaceView />
+    </Suspense>
+  );
 }
