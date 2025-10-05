9521ac4d9e4aba32d0dc97e90fa48a978f6073ed
Leo Li <leoli_082000@hotmail.com>
2025-10-05 01:55:08 -0400

    move sidebar data around

--- app/lib/sidebar-data.ts
+++ app/lib/sidebar-data.ts
@@ -1,5 +1,4 @@
 export const shortcuts = [
-  { label: "Add Item", href: "/space/add" },
   { label: "Easy Arrays", href: "/space?tags=leetcode+topic:array+diff:easy" },
   { label: "Google Hard", href: "/space?tags=leetcode+company:google+diff:hard" },
   { label: "Recently Updated", href: "/space?tags=leetcode+order:recent" },
