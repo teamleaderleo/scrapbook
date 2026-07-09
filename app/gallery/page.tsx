f84c5472d5bbd203bbf197cfd3388c533749e1cd
Leo <leoli_082000@hotmail.com>
2026-07-09 22:48:50 +0800

    Fix gallery nav stacking

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -3,10 +3,9 @@ import SiteNav from '@/components/site-nav';
 
 export default function GalleryPage() {
   return (
-    // TODO: fix the z level issue
     <div className="h-screen relative">
       <Scene3D />
-      <div className="absolute top-0 left-0 right-0 z-[0]">
+      <div className="absolute top-0 left-0 right-0 z-50">
         <SiteNav />
       </div>
     </div>
