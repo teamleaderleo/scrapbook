23665b97f53e59140077b07ad774b39ec85e5616
Leo <leoli_082000@hotmail.com>
2026-07-25 15:54:10 +0800

    Centralize space query fields

---
+++ app/lib/space-data.ts
@@ -0,0 +1,16 @@
+export const SPACE_PAGE_SIZE = 100;
+
+export const SPACE_ITEM_SELECT = [
+  'id',
+  'user_id',
+  'title',
+  'slug',
+  'url',
+  'default_index',
+  'versions',
+  'tags',
+  'category',
+  'score',
+  'created_at',
+  'updated_at',
+].join(',');
