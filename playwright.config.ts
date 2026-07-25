df7ecd990fed783844da32cc9693e4ad04054b63
Leo <leoli_082000@hotmail.com>
2026-07-26 03:14:40 +0800

    Run browser tests in Chromium and WebKit

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,6 +14,10 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
   webServer: {
     command: 'pnpm dev',
