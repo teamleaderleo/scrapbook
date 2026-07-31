0ecf1a0eee7dd37b7b5552f5c17f2e0de8f029bc
Leo <leoli_082000@hotmail.com>
2026-07-31 21:24:06 +0800

    Retire the disconnected legacy login surface

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/proxy-dashboard/', '/login/'],
+      disallow: ['/proxy-dashboard/'],
     },
     sitemap: 'https://teamleaderleo.com/sitemap.xml',
   };
